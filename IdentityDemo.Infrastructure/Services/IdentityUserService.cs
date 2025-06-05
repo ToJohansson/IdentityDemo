780b942bad895a4ba376b350077af7f898fb4136
Jörgen Hägglund <jorgen@sirlucifer.mine.nu>
2025-06-05 15:38:37 +0200

    Added Logout

--- IdentityDemo.Infrastructure/Services/IdentityUserService.cs
+++ IdentityDemo.Infrastructure/Services/IdentityUserService.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,15 +17,19 @@ public class IdentityUserService(UserManager<ApplicationUser> userManager,
 {
     public async Task<UserResultDto> CreateUserAsync(UserProfileDto user, string password)
     {
-        var result = await userManager.CreateAsync(new ApplicationUser
+        var _user = new ApplicationUser
         {
             UserName = user.Email,
             Email = user.Email,
             FirstName = user.FirstName,
             LastName = user.LastName,
-        },
-            password
+        };
+        var result = await userManager.CreateAsync(_user, password
         );
+        await userManager.AddClaimsAsync(_user, [
+            new Claim("firstName", _user.FirstName),
+            new Claim("lastName", _user.LastName)
+            ]);
         return new UserResultDto(result.Errors.FirstOrDefault()?.Description);
     }
 
@@ -33,4 +38,6 @@ public class IdentityUserService(UserManager<ApplicationUser> userManager,
         var result = await signInManager.PasswordSignInAsync(email, password, false, false);
         return new UserResultDto(result.Succeeded ? null : "Invalid user credentials");
     }
+
+    public async Task SignOutAsync() => await signInManager.SignOutAsync();
 }
